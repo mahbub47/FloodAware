1176b03f0063544a6ba2fd98c8610076eeae4741
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2024-06-23 04:43:55 +0600

    initial commit Home Screen UI done

---
+++ app/src/main/java/com/example/floodaware/Login.java
@@ -0,0 +1,34 @@
+package com.example.floodaware;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    TextView signupRedirect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        signupRedirect = findViewById(R.id.signupRedirectTV);
+        signupRedirect.setOnClickListener(v -> {
+            startActivity(new Intent(Login.this,Signup.class));
+            finish();
+        });
+    }
+}
